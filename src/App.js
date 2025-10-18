3c4dddb6a5ef9272ea02c5a94165a08c4ed65b5a
한정희 <jeo4924@gmail.com>
2025-10-18 21:13:14 +0900

    Add Link import to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ClubInvite from './components/ClubInvite';
 import InviteAccept from './components/InviteAccept';
 import WelcomePage from './components/WelcomePage';
